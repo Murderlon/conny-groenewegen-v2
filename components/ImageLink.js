042d1e7c239244d31da10bf6d2856d86dc90b0d1
Daan Rongen <daanrongen@hotmal.com>
2022-05-27 17:10:21 +0100

    changes

--- components/ImageLink.js
+++ components/ImageLink.js
@@ -2,19 +2,19 @@ import Link from 'next/link'
 
 export default function ImageLink({ href, title, image, color }) {
   return (
-    <li className="border-blue border-2">
-      <h2 className="text-xl font-bold">
+    <li className="border-blue w-64">
+      <h2 className="text-2xl font-bold relative top-4">
         RE-<span className={`text-${color} lowercase`}>{title}</span>
       </h2>
-      <img className="h-64 w-64 rounded-full" src={image} />
+      <img className="h-64 w-64 rounded-full shadow-inner" src={image} />
       <Link href={href}>
         <a
-          className={`relative left-20 rounded-sm bg-${color} p-2 text-sm uppercase text-black`}
+          className={`font-bold relative float-right bottom-10 right-4 rounded-sm bg-${color} p-2 text-sm uppercase text-black`}
         >
           Explore →
         </a>
       </Link>
-      <p>Couture</p>
+      <p className="mt-8 text-center uppercase text-sm font-bold  ">{title}</p>
     </li>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,44 @@
 import Script from 'next/script'
 import Link from 'next/link'
-import ImageLink from "../components/ImageLink"
+import ImageLink from '../components/ImageLink'
 
 export default function Home() {
   return (
     <>
       <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></Script>
-      <nav className="mx-auto mt-16 border-2 border-white md:max-w-max">
+      <nav className="mx-auto mt-16 border-white md:max-w-max">
         <ul className="md:flex md:space-x-20">
-          <ImageLink href="/re-couture" title="Couture" image="/assets/06-mg_4190-web.jpg" color='couture' />
-          <ImageLink href="/re-materialize" title="Materialize" image="/assets/02-nugget-coat.jpg" color='materialize' />
-          <ImageLink href="/re-space" title="Space" image="/assets/_05-hw_6119-proto-3-1.jpg" color='space' />
+          <ImageLink
+            href="/re-couture"
+            title="Couture"
+            image="/assets/06-mg_4190-web.jpg"
+            color="couture"
+          />
+          <ImageLink
+            href="/re-materialize"
+            title="Materialize"
+            image="/assets/02-nugget-coat.jpg"
+            color="materialize"
+          />
+          <ImageLink
+            href="/re-space"
+            title="Space"
+            image="/assets/_05-hw_6119-proto-3-1.jpg"
+            color="space"
+          />
         </ul>
       </nav>
 
-      <div className="min-w-1/3 mx-auto max-w-full">
-        <ul>
+      <div className="mx-auto min-w-min md:w-2/3">
+        <ul className="md:flex">
           <li>
             <h3>FaMa</h3>
-            <p>
+            <p className="mb-6">
               Fashion Machine questions the consequences of the fast fashion
               industry and its impact on our environment
             </p>
             <a
+              className="border-2 rounded-md p-2 text-sm font-bold uppercase"
               href="http://fashionmachine.org"
               target="_blank"
               rel="noreferrer"
@@ -32,8 +48,13 @@ export default function Home() {
           </li>
           <li>
             <h3>Electric Co</h3>
-            <p>3D soft tech knitwear designs</p>
-            <a href="https://electricco.co" target="_blank" rel="noreferrer">
+            <p className="mb-6">3D soft tech knitwear designs</p>
+            <a
+              className="border-2 rounded-md p-2 text-sm font-bold uppercase"
+              href="https://electricco.co"
+              target="_blank"
+              rel="noreferrer"
+            >
               Go to website
             </a>
           </li>
