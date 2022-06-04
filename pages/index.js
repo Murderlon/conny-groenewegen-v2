65ae82f397cc23f97a47d2068f9b1e921b75108a
Murderlon <merlijn@soverin.net>
2022-06-04 11:57:41 +0200

    Almost finish it

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,167 @@
 import Script from 'next/script'
 import Link from 'next/link'
-import ImageLink from '../components/ImageLink'
+import Image from 'next/image'
+import Head from 'next/head'
+
+import coutureImage from '../public/assets/06-mg_4190-web.jpg'
+import spaceImage from '../public/assets/_01-hw_6109-proto1-1.jpg'
+import materializeImage from '../public/assets/materialize-preview.jpg'
+
+function Category(props) {
+  const { category, textClass, bgClass, children } = props
+
+  return (
+    <li className="mx-auto md:mx-0 mt-12 w-[20em]">
+      <h2 className="relative top-4 z-10 -mb-4 font-mono text-4xl font-semibold">
+        RE-<span className={textClass}>{category}</span>
+      </h2>
+      <div className="image-shadow-inset">{children}</div>
+      <div className="relative z-10 -mt-4 flex justify-end">
+        <Link href={`/re-${category}`}>
+          <a
+            className={`mr-4 rounded ${bgClass} p-2 text-right uppercase text-black`}
+          >
+            Explore →
+          </a>
+        </Link>
+      </div>
+    </li>
+  )
+}
 
 export default function Home() {
   return (
-    <>
+    <div className="m-12 max-w-[80em] mx-auto">
       <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></Script>
-      <nav className="mx-auto mt-16 border-white md:max-w-max">
-        <ul className="md:flex md:space-x-20">
-          <ImageLink
-            href="/re-couture"
-            title="Couture"
-            image="/assets/06-mg_4190-web.jpg"
-            color="couture"
-          />
-          <ImageLink
-            href="/re-materialize"
-            title="Materialize"
-            image="/assets/02-nugget-coat.jpg"
-            color="materialize"
-          />
-          <ImageLink
-            href="/re-space"
-            title="Space"
-            image="/assets/_05-hw_6119-proto-3-1.jpg"
-            color="space"
-          />
-        </ul>
-      </nav>
 
-      <div className="mx-auto min-w-min md:w-2/3">
-        <ul className="md:flex">
-          <li>
-            <h3>FaMa</h3>
-            <p className="mb-6">
-              Fashion Machine questions the consequences of the fast fashion
-              industry and its impact on our environment
-            </p>
-            <a
-              className="border-2 rounded-md p-2 text-sm font-bold uppercase"
-              href="http://fashionmachine.org"
-              target="_blank"
-              rel="noreferrer"
+      <Head>
+        <title>Conny Groenewegen</title>
+      </Head>
+
+      <main>
+        <div className=''>
+        <h1 className="text-5xl lg:text-8xl font-semibold">Conny Groenewegen</h1>
+        <p className="my-4 text-lg">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        </div>
+
+        <nav>
+          <ul className="flex flex-col md:flex-row md:justify-between">
+            <Category
+              category="couture"
+              textClass="text-couture"
+              bgClass="bg-couture"
             >
-              Go to website
-            </a>
-          </li>
-          <li>
-            <h3>Electric Co</h3>
-            <p className="mb-6">3D soft tech knitwear designs</p>
-            <a
-              className="border-2 rounded-md p-2 text-sm font-bold uppercase"
-              href="https://electricco.co"
-              target="_blank"
-              rel="noreferrer"
+              <Image
+                alt="Fashion by Conny Groenewegen"
+                src={coutureImage}
+                layout="responsive"
+                width={400}
+                height={400}
+                objectFit="cover"
+                placeholder="blur"
+                style={{ borderRadius: '50%' }}
+              />
+            </Category>
+
+            <Category
+              category="space"
+              textClass="text-space"
+              bgClass="bg-space"
             >
-              Go to website
-            </a>
-          </li>
-        </ul>
-      </div>
-    </>
+              <Image
+                alt="Exhibitions by Conny Groenewegen"
+                src={spaceImage}
+                layout="responsive"
+                width={400}
+                height={400}
+                objectFit="cover"
+                placeholder="blur"
+                style={{ borderRadius: '50%' }}
+              />
+            </Category>
+
+            <Category
+              category="materialize"
+              textClass="text-materialize"
+              bgClass="bg-materialize"
+            >
+              <Image
+                alt="Fabrics by Conny Groenewegen"
+                src={materializeImage}
+                layout="responsive"
+                width={400}
+                height={400}
+                objectFit="cover"
+                placeholder="blur"
+                style={{ borderRadius: '50%' }}
+              />
+            </Category>
+          </ul>
+        </nav>
+
+        <div className="mx-auto md:mx-0 max-w-md">
+          <h2 className="mt-12 text-4xl">Organisations</h2>
+          <p className="my-2">something something explanation </p>
+        </div>
+
+        <div className="mx-auto md:mx-0 grid md:grid-cols-2 max-w-md md:max-w-2xl gap-4">
+          <article className="mt-12 flex gap-4">
+            <div>
+              <div className="video-shadow-inset relative h-24 w-24">
+                <video
+                  autoPlay
+                  loop
+                  className="h-full w-full self-start rounded-full object-cover"
+                  src="/assets/fama.mp4"
+                ></video>
+              </div>
+            </div>
+            <div>
+              <h3 className="text-xl font-semibold">FaMa</h3>
+              <p className="mb-6">
+                Fashion Machine questions the consequences of the fast fashion
+                industry and its impact on our environment
+              </p>
+              <a
+                className="rounded-md border-2 p-2 text-sm uppercase"
+                href="http://fashionmachine.org"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Go to website →
+              </a>
+            </div>
+          </article>
+
+          <article className="my-12 flex gap-4">
+            <div>
+              <div className="video-shadow-inset relative h-24 w-24">
+                <video
+                  autoPlay
+                  loop
+                  className="h-24 w-24 self-start rounded-full object-cover"
+                  src="/assets/electricco.mp4"
+                ></video>
+              </div>
+            </div>
+            <div>
+              <h3 className="text-xl font-semibold">Electric Co</h3>
+              <p className="mb-6">3D soft tech knitwear designs</p>
+              <a
+                className="rounded-md border-2 p-2 text-sm uppercase"
+                href="https://electricco.co"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Go to website →
+              </a>
+            </div>
+          </article>
+        </div>
+      </main>
+    </div>
   )
 }
